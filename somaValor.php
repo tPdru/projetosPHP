75d2e409bfe2daf7eb8277bdffbad6c060587069
tPdru <pedro.andrade64@etec.sp.gov.br>
2024-08-14 22:07:51 -0300

    Estrutura de decisao

---
+++ somaValor.php
@@ -0,0 +1,14 @@
+<?php 
+	$num1 = 20.50;
+	$num2 = 10.15;
+	$resp = 0.0;
+
+	$resp = $num1 + $num2;
+
+	if ($resp >= $num1) {
+		echo "Resposta é maior que num1";
+	}else{
+		echo "Resposta é menor que num1";
+	}
+
+ ?>
