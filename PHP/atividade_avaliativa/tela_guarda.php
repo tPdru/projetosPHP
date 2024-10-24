9c10413640add5e7c10607124148ab8cbcfd91b9
tPdru <pedro.andrade64@etec.sp.gov.br>
2024-10-23 22:13:00 -0300

    pag 1 e pag 2 prontas

--- PHP/atividade_avaliativa/tela_guarda.php
+++ PHP/atividade_avaliativa/tela_guarda.php
@@ -14,13 +14,25 @@
 		$_SESSION['peso'] = $_POST['peso'];
 		$_SESSION['altura'] = $_POST['altura'];
 
+		$index = 0;
+		$list_nomes =[];
+		$list_idade =[];
+		$list_peso =[];
+		$list_altura =[];
+		
 
-
+		$list_nomes[$index] = $_SESSION['nome'];
+		$list_idade[$index]	= $_POST['idade'];
+		$list_peso[$index]	= $_POST['peso'];
+		$list_altura[$index] = $_POST['altura'];
+		$index++;
+		header("Location: tela_cadastro.php");
+		exit();
 	?>
 </head>
 <body>
 
-	<b>Funcionário: </b> <?php echo $_SESSION["nome"]; ?>, logado com sucesso.<br>
+	
 
 </body>
 </html>
